b4e817e088a6209737d38386615a687284dac048
Eustrain <eustrain.rs@gmail.com>
2019-06-19 22:30:04 -0500

    Function

--- Augmented Design 1.R
+++ Augmented Design 1.R
@@ -1,16 +1,8 @@
+
+##***********Augmented Desing************###
+
 library(plyr)
 
-blk <- c(1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3)
-trt <- c(1, 2, 3, 4, 7, 11, 12, 1, 2, 3, 4, 5, 9, 1, 2, 3, 4, 8, 6, 10)
-y1 <- c(92, 79, 87, 81, 96, 89, 82, 79, 81, 81, 91, 79, 78, 83, 77, 78, 78,
-        70, 75, 74)
-y2 <- c(258, 224, 238, 278, 347, 300, 289, 260, 220, 237, 227, 281, 311, 250,
-        240, 268, 287, 226, 395, 450)
-
-#####Example 2
-#####dat<- data.frame(blk,trt,y1)
-####str(dat)
-#####names(dat) <- c("block","Trat","y")
 
 dat <- read.csv("data.csv",head=T)
 head(dat)
@@ -18,7 +10,14 @@ str(dat)
 
 dat$block <- as.factor(dat$block)
 dat$Trat <- as.factor(dat$Trat)
+
+Aug_dis(dat$block,dat$Trat,dat$y,dat)
+
+Aug_dis<- function(block,Trat,y,dat) {
+  
+
 ####################### analysis of variance 
+
 fm1 <-lm(y~Trat+block,data = dat)
 av <- anova(fm1)
  
@@ -38,13 +37,13 @@ su= sum(y))
 nblock <- length(levels(dat$block))
 
 ######################################################
-##### 
+#####  Sums sq and Mean sq for checks
 c <- length(cdata$Trat)
-cFC <- sum(checks$y)^2/(3*c)         #(nblock*c)
-css <- sum(cdata$su^2)/3-cFC       #nblock -cFC
-cms <- css/(c-1) ####### <- c-1 c= chechks 
+cFC <- sum(checks$y)^2/(nblock*c)         
+css <- sum(cdata$su^2)/nblock-cFC       
+cms <- css/(c-1) ####### 
 #######################
-######Sums sq and Mean sq for  check
+######Sums sq and Mean sq for  varieties
 vcf <- sum(variedades$y)^2/ length(variedades$y)
 vss <- sum(variedades$y^2)-vcf
 vms <- vss/(length(variedades$y)-1)
@@ -83,26 +82,55 @@ for (i in 1:3) {
 anv <- data.frame("."=fv,"Df"=df,"Sum sq"=ss,"Mean sq"=ms
                   ,"F value"=f,"Pr(>F)"=fval)
 
-result <- list(anv,av)
+
 ########################################################
 ####Standard Error of mean difference#######
-fm3 <- lm(y~Trat,data = dat)
-av2 <- anova(fm3)
+
 #################between 2 checks
-SEC <-( sqrt(2* av2$`Mean Sq`[2]))/sqrt((3))      ###nblock
+SEC <- sqrt(2 * av$`Mean Sq`[3]/nblock)    
 #####SEC <-( sqrt(2* av$`Mean Sq`[3]))/sqrt((nblock)) 
 ########################between 2 vars
-SEV <-sqrt( (2* av2$`Mean Sq`[2]))
+SEV <-sqrt( (2* av$`Mean Sq`[3]))
 ########SEV <- sqrt( (2* av$`Mean Sq`[3]))
 
-################between a check
-SE2 <- sqrt(av2$`Mean Sq`[2]*(1+(1/3)))
+################between between any tw~ entries of the same block
+SE2 <- sqrt(av$`Mean Sq`[3]*1+(1/c))
+
+#############between means of a chec+ and a test variety)
+SE3 <- sqrt(av$`Mean Sq`[3]*(1+1/nblock+1/c+1/(nblock*c)))
+
 ##################between a vars
-SEV2 <- sqrt( 2.9*1.333333)
+Err_st <-c(SEC,SEV,SE2,SE3 )
+er <- c("between 2 checks means","between 2 Test"," two entries of the same block","Test Treatment and a Control Treatment")
+DF <- data.frame("."=er,"Std. Error of Diff"=Err_st)
+###########################
+result_anv <- list(anv,av)
+result_est <- list(DF)
+re<- list(result_anv,result_est)
+print(re)
 
 
+}
+
+#blk <- c(1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3)
+#y1 <- c(92, 79, 87, 81, 96, 89, 82, 79, 81, 81, 91, 79, 78, 83, 77, 78, 78,
+#trt <- c(1, 2, 3, 4, 7, 11, 12, 1, 2, 3, 4, 5, 9, 1, 2, 3, 4, 8, 6, 10)
+#70, 75, 74)
+#y2 <- c(258, 224, 238, 278, 347, 300, 289, 260, 220, 237, 227, 281, 311, 250,
+#240, 268, 287, 226, 395, 450)
+
+#####Example 2
+dat<- data.frame(blk,trt,y1)
+str(dat)
+names(dat) <- c("block","Trat","y")
+dat$block <- as.factor(dat$block)
+dat$Trat <- as.factor(dat$Trat)
+Aug_dis(dat$block,dat$Trat,dat$y,dat)
 ######Sharma, J.R. (1998) Statistical and Biometrical Techniques in Plant Breeding
-######
+###### Aravind, J., Mukesh Sankar, S., Wankhede, D. P., and Kaur, V.
+#(2019).  augmentedRCBD: Analysis of Augmented Randomised
+#Complete Block Designs. R package version 0.1.0.9000,
+#https://aravind-j.github.io/augmentedRCBD/https://cran.r-project.org/package=augmentedRCBD.
 
 
 
