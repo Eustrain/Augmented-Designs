8dbda9c338fe59dfcf726f61e9a4bfe4d3036f77
Eustrain <eustrain.rs@gmail.com>
2019-07-02 19:35:52 -0500

    Update Augmented Design 1.R

--- Augmented Design 1.R
+++ Augmented Design 1.R
@@ -8,37 +8,40 @@ dat <- read.csv("data.csv",head=T)
 head(dat)
 str(dat)
 
-dat$block <- as.factor(dat$block)
-dat$Trat <- as.factor(dat$Trat)
 
-Aug_dis(dat$block,dat$Trat,dat$y,dat)
 
-Aug_dis<- function(block,Trat,y,dat) {
-  
+Aug_dis(dat$blk,dat$trt,dat$y1,dat)
 
+Aug_dis<- function(Block,Trat,y,dat) {
+  Block <- as.factor(Block)
+  Trat <- as.factor(Trat) 
+  y <- as.numeric(y) 
+###############################
+ 
 ####################### analysis of variance 
 
-fm1 <-lm(y~Trat+block,data = dat)
+fm1 <-aov(y~Trat+Block)
 av <- anova(fm1)
  
 #####################################################
 ###Calculate the  frequency for the varieties and checks
-trats<- data.frame(table(dat$Trat))
-vari = as.vector (trats$Var1[trats$Freq == length(3)])
-chec = as.vector (trats$Var1[trats$Freq ==3])
+trats<- data.frame(table(Trat))
+vari = as.vector (trats$Trat[trats$Freq == length(3)])
+chec = as.vector (trats$Trat[trats$Freq ==3])
 ######################################################
 variedades <- filter(dat, Trat %in% vari)
 checks <- filter(dat, Trat %in% chec)
+names(checks) <- c("Block","Trat","y")
 ##################################################
-
 cdata <- ddply(checks, c("Trat"),summarise,
 su= sum(y))
 ######################################################
-nblock <- length(levels(dat$block))
+nblock <- length(levels(Block))
+c <- length(cdata$Trat)
 
 ######################################################
 #####  Sums sq and Mean sq for checks
-c <- length(cdata$Trat)
+
 cFC <- sum(checks$y)^2/(nblock*c)         
 css <- sum(cdata$su^2)/nblock-cFC       
 cms <- css/(c-1) ####### 
@@ -80,7 +83,7 @@ for (i in 1:3) {
 ########################################
 ##########ANOVA
 anv <- data.frame("."=fv,"Df"=df,"Sum sq"=ss,"Mean sq"=ms
-                  ,"F value"=f,"Pr(>F)"=fval)
+                  ,"F value"=f,"Prob"=fval)
 
 
 ########################################################
@@ -104,36 +107,33 @@ Err_st <-c(SEC,SEV,SE2,SE3 )
 er <- c("between 2 checks means","between 2 Test"," two entries of the same block","Test Treatment and a Control Treatment")
 DF <- data.frame("."=er,"Std. Error of Diff"=Err_st)
 ###########################
-me_cv<- ddply(dat, c("Trat"),summarise,
-             means= mean(y))
+me_cv<- tapply(y, Trat, mean)
+me_cv <- as.data.frame(me_cv)
+names(me_cv) <- c("Means")
 ###########################
-result_anv <- list(anv,av)
-result_est <- list(DF)
-re<- list(result_anv,result_est)
-print(re)
-print(me_cv)
 
+result<-list(ANOVA=anv,av,Standard_Error=DF,Means=me_cv )
+print(result)
 }
 
 #blk <- c(1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3)
-#y1 <- c(92, 79, 87, 81, 96, 89, 82, 79, 81, 81, 91, 79, 78, 83, 77, 78, 78,
 #trt <- c(1, 2, 3, 4, 7, 11, 12, 1, 2, 3, 4, 5, 9, 1, 2, 3, 4, 8, 6, 10)
-#70, 75, 74)
+#y1 <- c(92, 79, 87, 81, 96, 89, 82, 79, 81, 81, 91, 79, 78, 83, 77, 78, 78,
+ #       70, 75, 74)
+
 #y2 <- c(258, 224, 238, 278, 347, 300, 289, 260, 220, 237, 227, 281, 311, 250,
 #240, 268, 287, 226, 395, 450)
 
 #####Example 2
 #dat<- data.frame(blk,trt,y1)
-#names(dat) <- c("block","Trat","y")
-#str(dat)
-#dat$block <- as.factor(dat$block)
-#Aug_dis(dat$block,dat$Trat,dat$y,dat)
-#dat$Trat <- as.factor(dat$Trat)
+
+
 ######Sharma, J.R. (1998) Statistical and Biometrical Techniques in Plant Breeding
 ###### Aravind, J., Mukesh Sankar, S., Wankhede, D. P., and Kaur, V.
 #(2019).  augmentedRCBD: Analysis of Augmented Randomised
 #Complete Block Designs. R package version 0.1.0.9000,
-#https://aravind-j.github.io/augmentedRCBD/https://cran.r-project.org/package=augmentedRCBD.
+#https://aravind-j.github.io/augmentedRCBD/
+##https://cran.r-project.org/package=augmentedRCBD.
 
 
 
